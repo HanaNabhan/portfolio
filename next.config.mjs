efa6bc0810725a34836a51c54a998ee505f2d600
hana ahmed nabhan ouda <hona.ahmed1@gmail.com>
2025-07-30 14:04:34 +0300

    new

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
